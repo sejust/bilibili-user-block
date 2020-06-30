9c62b9c550efb6201eab6dd5066595eba6da01e8
slasher <mcq.sejust@gmail.com>
2020-06-30 17:43:48 +0800

    v0.0.1

---
+++ Bilibili用户屏蔽.user.js
@@ -0,0 +1,67 @@
+// ==UserScript==
+// @name                Bilibili用户屏蔽
+// @name:zh-CN          Bilibili用户屏蔽
+// @namespace           http://tampermonkey.net/
+// @version             0.0.1
+// @description         try to take over the world!
+// @description:zh-CN   屏蔽在线和排行中不喜欢的UP主
+// @author              Slasher Liang
+// @match               *://www.bilibili.com/video/online*
+// @match               *://www.bilibili.com/ranking*
+// @grant               none
+// ==/UserScript==
+
+const blockedUserOnline = [
+];
+
+const blockedUserRank = [
+];
+
+const blockedUserAll = [
+];
+
+(function () {
+
+  // online
+  const onlineList = document.querySelectorAll('.online-list>div');
+  for (let i = 0; i < onlineList.length; i++) {
+    const d = onlineList[i];
+    const user = d.querySelector('.dlo>a');
+    if (user) {
+      const parts = user.href.split('/');
+      const uid = parts[parts.length - 2];
+      if (blockedUserAll.includes(uid) || blockedUserOnline.includes(uid)) {
+        console.log('online blocked: ' + uid);
+        d.remove();
+      }
+    }
+  }
+
+  function checkAndBlockRank() {
+    // ranking
+    const rankList = document.querySelectorAll('.rank-list>li');
+    for (let j = 0; j < rankList.length; j++) {
+      const d = rankList[j];
+      const user = d.querySelector('.detail>a');
+      if (user) {
+        const parts = user.href.trimEnd().split('/');
+        const uid = parts[parts.length - 1];
+        if (blockedUserAll.includes(uid) || blockedUserRank.includes(uid)) {
+          console.log('ranking blocked: ' + uid);
+          d.remove();
+        }
+      }
+    }
+  }
+
+  // run first time
+  checkAndBlockRank();
+  // tab click
+  const tabs = document.getElementsByClassName('rank-tab-wrap')[0];
+  tabs &&
+    tabs.addEventListener('click', function () {
+      console.log('clicked');
+      setTimeout(checkAndBlockRank, 300);
+    });
+
+})();
--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # Bilibili 屏蔽用户
+屏蔽`当前在线` `排行榜`中个人不喜欢的up主
 
+## 使用方法
+该脚本依赖于为 [Tampermonkey](http://www.tampermonkey.net);
+在Tampermonkey中新建脚本，把本例脚本复制进去即可
+
+## 添加屏蔽用户
+目前只支持修改脚本
+
+```
+// 单独只在online页面屏蔽
+const blockedUserOnline = [
+  '123',
+];
+// 单独只在ranking页面屏蔽
+const blockedUserRank = [
+  '456',
+];
+// 所有页面均屏蔽
+const blockedUserAll = [
+  '999',
+];
+```
